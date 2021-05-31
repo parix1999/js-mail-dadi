4ec3bbd56523852d2c8e13c071df0834df431e86
parix1999 <parisottoedoardo@gmail.com>
2021-05-31 15:16:30 +0200

    emailDadi-1.0

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// esercizio email
+// steps= Creare un input con un prompt e una Array con all'interno
+// delle email scritte. Per poi farle iterare e controllare da un 
+// ciclo for.
+
+var inputEmail = prompt('Ciao inserisci la tua email per vedere se è dentro al nostro database');
+
+var listaEmail = ['gianni@gmail.com', 'marco@yahoo.it', 'edoardo@gmail.com', 'piero@outlook.it', 'giada@libero.it', 'laura@icloud.com'];
+console.log(listaEmail);
+console.log(listaEmail[2]);
+
+
